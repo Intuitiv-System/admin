985a23dbfc1948ed2dfe84166eed8b375c984259
Xx-KaAzZ-xX <Xx-KaAzZ-xX@users.noreply.github.com>
2016-07-13 10:12:40 +0200

    Update deb8Temp.sh
    Former-commit-id: 99ff27b8fc1abba3457e2893e3576e918e3b7a53

--- Shell/deb8Temp.sh
+++ Shell/deb8Temp.sh
@@ -77,6 +77,12 @@ apt-get update && apt-get upgrade
 apt-get install apache2 php5 php5-gd php5-intl php5-xsl php5-mcrypt php5-memcached php5-fpm php5-curl curl imagemagick php5-imagick
 a2enmod actions headers expires rewrite
 
+##Faire en sorte que le logrotate d'apache soit le dernier a être exécuté
+if [[ -f /etc/logrotate.d/apache2]]
+then
+mv /etc/logrotate.d/apache2 /etc/logrotate.d/z_apache2
+fi
+
 # Custom PHP
 echo ";
 ;;;; Custom of php.ini
