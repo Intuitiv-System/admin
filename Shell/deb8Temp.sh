1b169feaa9807eea51dd72b730e6255f8c057eb1
Mathieu <mandroz@intuitiv.fr>
2016-08-02 12:01:37 +0200

    news
    Former-commit-id: bd9c148db4fbc5b701f1752283d70e3e9362c2a9

--- Shell/deb8Temp.sh
+++ Shell/deb8Temp.sh
@@ -78,7 +78,7 @@ apt-get install apache2 php5 php5-gd php5-intl php5-xsl php5-mcrypt php5-memcach
 a2enmod actions headers expires rewrite
 
 ##Faire en sorte que le logrotate d'apache soit le dernier a être exécuté
-if [[ -f /etc/logrotate.d/apache2]]
+if [[ -f /etc/logrotate.d/apache2 ]]
 then
 mv /etc/logrotate.d/apache2 /etc/logrotate.d/z_apache2
 fi
