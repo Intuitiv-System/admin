4dd870b7c11e6cc77811c885cfc05664c03cb8af
Xx-KaAzZ-xX <Xx-KaAzZ-xX@users.noreply.github.com>
2016-09-16 14:59:12 +0200

    Add files via upload
    Former-commit-id: 724db50ae7a2e1019359d6dcbd7788bf5639e5b9

--- Shell/mysql/newDB.sh
+++ Shell/mysql/newDB.sh
@@ -2,64 +2,30 @@
 
 ## Création BDD et USER
 
-S1='utf8'
-S2='utf8mb4'
-
-if [ -z "$1" ]
+if [[ -z "$1" ]] || ( [[ "$1" != "utf8" ]] || [[ "$1" != "utf8mb4" ]] )
 then
   echo -e "Usage:
-  ./newDB.sh utf8
-  or
-  ./newDB.sh utf8mb4"
-exit 1
+  ./newDB.sh {utf8|utf8mb4}
+  "
+  exit 1
 fi
 
-echo -e "Enter un nom d'utilisateur SQL\n"
-read username
+read -p  "Enter un nom d'utilisateur SQL : " username
 
-if [ "$1" = "$S1" ]
-then
-  echo "Password for SQL user: ${username}"
-  read -s password
-  echo "Confirmer le mot de passe SQL pour ${username}"
-  read -s password2
-  while [ "${password}" != "${password2}" ]
-    do
-      echo -e "Password don't match\n"
-      echo "Password for SQL user: ${username}"
-      read -s password
-      echo "Confirmer le mot de passe SQL pour ${username}"
-      read -s password2
-    done
-     Q1="CREATE DATABASE IF NOT EXISTS ${username} CHARACTER SET utf8;"
-     Q2="CREATE USER '${username}'@'localhost' IDENTIFIED BY '${password}';"
-     Q3="GRANT ALL PRIVILEGES ON ${username}.* TO '${username}'@'localhost';"
-     Q4="FLUSH PRIVILEGES;"
-     SQL="${Q1}${Q2}${Q3}${Q4}"
+read -s -p "Password for SQL user: ${username} : " password 
+read -s -p "Confirmer le mot de passe SQL pour ${username} : " password2
+while [ "${password}" != "${password2}" ]
+  do
+    echo -e "Password don't match\n"
+    read -s -p "Password for SQL user: ${username}" password
+    read -s -p "Confirmer le mot de passe SQL pour ${username}" password2
+  done
 
-     mysql -u root -p -e "${SQL}"
-elif [ "$1" = "$S2" ]
-then
-   echo "Password for SQL user: ${username}"
-   read -s password
-   echo "Confirmer le mot de passe SQL pour ${username}"
-   read -s password2
-   while [ "${password}" != "${password2}" ]
-     do
-      echo -e "Password don't match\n"
-      echo "Password for SQL user: ${username}"
-      read -s password
-      echo "Confirmer le mot de passe SQL pour ${username}"
-      read -s password2
-     done
-  Q1="CREATE DATABASE IF NOT EXISTS ${username} CHARACTER SET utf8mb4;"
-  Q2="CREATE USER '${username}'@'localhost' IDENTIFIED BY '${password}';"
-  Q3="GRANT ALL PRIVILEGES ON ${username}.* TO '${username}'@'localhost';"
-  Q4="FLUSH PRIVILEGES;"
-  SQL="${Q1}${Q2}${Q3}${Q4}"
-
-  mysql -u root -p -e "${SQL}"
-
-fi
+Q1="CREATE DATABASE IF NOT EXISTS ${username} CHARACTER SET ${1};"
+Q2="CREATE USER '${username}'@'localhost' IDENTIFIED BY '${password}';"
+Q3="GRANT ALL PRIVILEGES ON ${username}.* TO '${username}'@'localhost';"
+Q4="FLUSH PRIVILEGES;"
+SQL="${Q1}${Q2}${Q3}${Q4}"
+mysql -u root -p -e "${SQL}"
 
 exit 0
