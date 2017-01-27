3cc28e6a84b2bb23eb0548c08d1caa02f2eb5e2e
root <root@smtp.itserver.fr>
2017-01-27 14:05:52 +0100

    reste à formater le fichier final pour logstash

--- Shell/postfix-bounce.sh
+++ Shell/postfix-bounce.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
-allBounced=$(cat /var/log/mail.log | grep status=bounced)
+#allBounced=$(cat /var/log/mail.log | grep status=bounced)
 messages_id=$(cat /var/log/mail.log | grep status=bounced | awk '{print $6}' | sed 's/://')
 nbBounced=$(cat /var/log/mail.log | grep status=bounced | wc -l)
 file="/tmp/messages_id.txt"
 senders="/tmp/senders.txt"
 log_file="/tmp/bounces.log"
+final_file="/root/bounces.log"
+date_debut=$(cat /var/log/mail.log | head -n 1 | awk '{$1; $2; NF=2; print}')
+date_fin=$(tac /var/log/mail.log | head -n 1 | awk '{$1; $2; NF=2; print}')
 
   echo -e "${messages_id}\n" > ${file}
   sed  '/^$/d' ${file} > /tmp/tmp.txt && mv /tmp/tmp.txt ${file}
-
+  
 ##On récupère les NDD qui bounce
 cat ${file} | while read line
 do
@@ -28,8 +31,10 @@ do
   echo -e "${line} \t ${occurences} \t \t ${stats}%" >> ${log_file}
 done
 
-echo -e "Adresses \t Nombre de Bounces \t Pourcentage" > /root/bounces.log
-echo -e "--------------------------------------------" >> /root/bounces.log
+echo "Rapport de bounce du ${date_debut} au ${date_fin}" > ${final_file}
+echo -e "--------------------------------------------------------------|
+        Adresses \t Nombre de Bounces \t Pourcentage  |" >> ${final_file}
+echo -e "--------------------------------------------------------------|" >> ${final_file}
 cat ${log_file} | sort -u >> /root/bounces.log
 
 ##On fait un peu le ménage
